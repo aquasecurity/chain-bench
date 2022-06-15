5c27b1229f8f1dcd495fb20748c7f6bcf9093580
Mor Weinberger <morwnbrg@gmail.com>
2022-06-15 11:52:30 +0300

    seperate 4.2 from pacakgesregistry

---
+++ internal/checks/artifacts/access-to-artifacts/access_to_artifacts.go
@@ -0,0 +1,40 @@
+package accesstoartifacts
+
+import (
+	_ "embed"
+	"encoding/json"
+
+	"github.com/aquasecurity/chain-bench/internal/checks/common"
+	"github.com/aquasecurity/chain-bench/internal/models/checkmodels"
+)
+
+var (
+	//go:embed rules.metadata.json
+	metadataString []byte
+
+	//go:embed rules.rego
+	regoQuery string
+
+	checksMetadata checkmodels.CheckMetadataMap
+
+	checks = []*checkmodels.Check{}
+)
+
+func init() {
+	if err := json.Unmarshal(metadataString, &checksMetadata); err != nil {
+		panic(err)
+	}
+
+	if err := common.AppendCheck(&checks,
+		checkmodels.Check{
+			Action:           common.GetRegoRunAction(regoQuery, checksMetadata),
+			CheckMetadataMap: checksMetadata,
+		},
+	); err != nil {
+		panic(err)
+	}
+}
+
+func GetChecks() []*checkmodels.Check {
+	return checks
+}
