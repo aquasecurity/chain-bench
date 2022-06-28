b91a353a7f0096e7eaaa7f5ef00dc5d80e846d2d
Rémy Greinhofer <remy.greinhofer@gmail.com>
2022-06-28 11:54:14 -0500

    Add metedata to the report file
    This patch adds a new metadata section to the report file being
    generated by chain-bench.
    
    This initial version adds the generation date, and the check statistics
    which is useful to compare different scans and their evolutions over
    time.
    
    Fixes aquasecurity/chain-bench#38

--- internal/printer/helpers.go
+++ internal/printer/helpers.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"io/ioutil"
 	"os"
+	"time"
 
 	"github.com/aquasecurity/chain-bench/internal/models/checkmodels"
 )
@@ -16,10 +17,12 @@ var (
 )
 
 type Statistics struct {
-	Passed int
-	Failed int
+	Passed  int
+	Failed  int
+	Unknown int
+	Total   int
 }
-type displayResult struct {
+type reportResult struct {
 	ID          string
 	Name        string
 	Descrition  string
@@ -29,23 +32,53 @@ type displayResult struct {
 	Url         string
 }
 
+type reportMetadata struct {
+	Date       string
+	Statistics Statistics
+}
+
+type reportResults struct {
+	Metadata reportMetadata
+	Results  []reportResult
+}
+
 // println prints a string to the current configured output
 func println(msg string) {
 	fmt.Fprintln(output, msg)
 }
 
 func PrintOutputToFile(data []checkmodels.CheckRunResult, outputFilePath string) {
-	file, _ := json.MarshalIndent(getPrintFormat(data), "", "")
+	reportRes, statistics := getPrintFormat(data)
+
+	// Populate the report metadata.
+	reportMetadata := reportMetadata{
+		Date:       time.Now().Format(time.RFC3339),
+		Statistics: statistics,
+	}
+
+	// Populate the report.
+	report := reportResults{
+		reportMetadata,
+		reportRes,
+	}
+	file, _ := json.MarshalIndent(report, "", "")
 	err := ioutil.WriteFile(outputFilePath, file, 0644)
 	if err != nil {
 		PrintError("Failed to write to output file, make sure your path is valid")
 	}
 }
 
-func getPrintFormat(results []checkmodels.CheckRunResult) []displayResult {
-	resultsToDisplay := []displayResult{}
+func getPrintFormat(results []checkmodels.CheckRunResult) ([]reportResult, Statistics) {
+	resultsToDisplay := []reportResult{}
+	statistics := Statistics{
+		Passed:  0,
+		Failed:  0,
+		Unknown: 0,
+		Total:   len(results),
+	}
+
 	for _, r := range results {
-		resultsToDisplay = append(resultsToDisplay, displayResult{
+		resultsToDisplay = append(resultsToDisplay, reportResult{
 			Name:        r.Metadata.Title,
 			ID:          r.ID,
 			Descrition:  r.Metadata.Description,
@@ -53,13 +86,22 @@ func getPrintFormat(results []checkmodels.CheckRunResult) []displayResult {
 			Result:      string(r.Result.Status),
 			Reason:      r.Result.Details,
 			Url:         r.Metadata.Url})
+
+		switch r.Result.Status {
+		case "Passed":
+			statistics.Passed += 1
+		case "Failed":
+			statistics.Failed += 1
+		case "Unknown":
+			statistics.Unknown += 1
+		}
 	}
 
-	return resultsToDisplay
+	return resultsToDisplay, statistics
 }
 
 func initializeStatistics() Statistics {
-	return Statistics{Passed: 0, Failed: 0}
+	return Statistics{Passed: 0, Failed: 0, Unknown: 0, Total: 0}
 }
 
 func addToStatistics(s *Statistics, r checkmodels.ResultStatus) {
